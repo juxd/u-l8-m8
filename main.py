3b3177bcf219f8931140cf5b7f1635b4a4398cf7
micklwl <micklwl@hotmail.com>
2018-09-08 18:07:20 +0800

    commented

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from credentials import api_key
 from init import *
+import telegram
+import config
+import logging
 from telegram.ext import Updater
 from telegram.ext import MessageHandler, CommandHandler, Filters
 
 # Set up bot to start listening.
-bot = telegram.Bot(token=config.token)
+bot = telegram.Bot(token=api_key)
 updater = Updater(token=config.token)
 dispatcher = updater.dispatcher
 print("Bot started.") # Init message.
@@ -12,19 +15,30 @@ print("Bot started.") # Init message.
 
 def addInput(bot, update):
     eventName = update.message.text.replace("/add ", "")
-    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message in this format: DD/MM/YY HH:MM Location",
+    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's date in this format: DD/MM/YY",
                     parse_mode="Markdown")
-    dispatcher.add_handler(detailsHandler)
+    dispatcher.add_handler(dateHandler)
 
-def collectDetails:
-    details = update.message.text;
-    detailsList = details.split(" ")
-    date = detailsList[0]
-    time = detailsList[1]
-    place = detailsList[2]
+def collectDate(bot, update):
+    date = update.message.text;
+    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's time in this format: HH:MM",
+                parse_mode="Markdown")
+    dispatcher.add_handler(timeHandler)
     
+def collectTime(bot, update):
+    time = update.message.text;
+    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the location of meeting",
+                parse_mode="Markdown")
+    dispatcher.add_handler(locationHandler)
+
+def collectLocation(bot, update):
+    #somehow get one location
+    #location = 
+
 addHandler = CommandHandler('add', addInput)
-detailsHandler = MessageHandler(Filters.reply, collectDetails)
+dateHandler = MessageHandler(Filters.reply, collectDate)
+timeHandler = MessageHandler(Filters.reply, collectTime)
+locationHandler = MessageHandler(Filters.reply, collectLocation)
 
 dispatcher.add_handler(addHandler)
 updater.start_polling()
