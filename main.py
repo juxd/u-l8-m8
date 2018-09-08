8d53aef372d7dc0995b6b19c1636be66d1f2fa09
micklwl <micklwl@hotmail.com>
2018-09-08 17:17:45 +0800

    added first handler

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from credentials import api_key
 from init import *
 from telegram.ext import Updater
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, CommandHandler, Filters
 
 # Set up bot to start listening.
-bot = get_bot()
-updater = Updater(token=api_key)
-updater.start_polling()
+bot = telegram.Bot(token=config.token)
+updater = Updater(token=config.token)
+dispatcher = updater.dispatcher
 print("Bot started.") # Init message.
+
+
+def addInput(bot, update):
+  msgText = update.message.text.replace("/add ", "")
+  if (Error.error(msgText)):
+     bot.send_message(chat_id=update.message.chat_id, text = "Error in Adding! Try Again!",
+                    parse_mode="Markdown")
+  else:
+    eventName = LogicHandler.stringToFn(Parser.textToLogic(msgText))
+    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with this format:DD/MM/YY HH:MM Location",
+                    parse_mode="Markdown")
+    
+def collectMsg(bot, update):
+    
+
+addHandler = CommandHandler('add', addInput)
+
+dispatcher.add_handler(addHandler)
+updater.start_polling()
