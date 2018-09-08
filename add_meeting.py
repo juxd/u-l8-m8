efaf687df33d308e517218ec1b4b02a4dce06db7
remo5000 <vigneshshankar@gmail.com>
2018-09-08 21:25:55 +0800

    Add memory store to keep track of conversation

--- add_meeting.py
+++ add_meeting.py
@@ -2,27 +2,37 @@ from telegram.ext import CommandHandler, ConversationHandler, Filters, MessageHa
 
 WAITING_DATE, WAITING_TIME, WAITING_LOCATION = range(3)
 
+# Keep meeting information in memory
+store = {
+        'event_name': None,
+        'meeting_time': None,
+        'meeting_date': None,
+        'lat': None,
+        'lon': None
+        }
+
 def add_meeting(bot, update):
-    event_name = update.message.text.replace("/add ", "")
+    store.set('event_name', update.message.text.replace("/add ", ""))
     bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's date in this format: DDMMYY",
                     parse_mode="Markdown")
     return WAITING_DATE
 
 def input_date(bot, update):
-    date = update.message.text
+    store.set('meeting_date', update.message.text)
     bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's time in this format: HHMM",
                 parse_mode="Markdown")
     return WAITING_TIME
 
 def input_time(bot, update):
-    time = update.message.text
+    store.set('meeting_time', update.message.text)
     bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with attachment of the location",
                 parse_mode="Markdown")
     return WAITING_LOCATION
 
 def input_location(bot, update):
-    longitude = update.message.location.longitude
-    lantitude = update.message.location.latitude
+    store.set('longitude', update.message.location.longitude)
+    store.set('lantitude', update.message.location.latitude)
+    insert_meeting(update.message.chat.id)
     return
 
 add_meeting_handler = ConversationHandler(
