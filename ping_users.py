e1ce2c439e30dde6a3fed644a05e7a3318317fae
luwu <luwu@paypal.com>
2018-09-09 03:30:29 +0800

    update the determine if user is going to be late part

--- ping_users.py
+++ ping_users.py
@@ -1,4 +1,6 @@
 import db_manager
+import maps_api
+import time
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Filters, MessageHandler
 
@@ -11,12 +13,17 @@ def ping_for_loc(bot, meeting_id):
 def send_ping_to_person(dispatcher, username, meeting_id):
     location_button = KeyboardButton("Send Location", request_location=True)
     msg = "Where u @ my nibba?"
-    bot.send_message(username, msg, reply_markup=ReplyKeyboardMarkup([location_button]))
+    dispatcher.bot.send_message(username, msg, reply_markup=ReplyKeyboardMarkup([location_button]))
 
-def store_location(bot, update):
+def determine_if_user_being_late(bot, update):
     username = update.message.from_user.username
     location = update.message.location
-    meeting_id = db_manager.find_user_latest_meeting(user_id)
-    db_manager.update_user_location(username, location.latitude, location.longitude)
+    meeting_id = db_manager.find_user_latest_meeting(username)
+    travel_time = maps_api.get_travel_time(meeting_id, location)
+    travel_time *= 60
+    meeting_time = db_manager.get_meeting_time(meeting_id)
+    if travel_time > (meeting_time - int(time.time())):
+        db_manager.update_is_late(meeting_id, username, True)
 
-user_handler = MessageHandler(Filters.location, callback=store_location)
+
+user_handler = MessageHandler(Filters.location, callback=determine_if_user_being_late)
