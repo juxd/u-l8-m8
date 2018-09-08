7ef1f93b4f1fa8f906e9218774a6ac532f9a0a26
luwu <luwu@paypal.com>
2018-09-08 19:26:04 +0800

    add in some more functions

--- db_manager.py
+++ db_manager.py
@@ -49,12 +49,111 @@ def get_meeting_time(meetingID):
 
         c.execute("SELECT meetingTime FROM meetingTab WHERE meetingID=?", meetingID)
         meetingTime = c.fetchone()[0]
+        conn.close()
         print(meetingTime)
 
         return meetingTime
     except Exception as e:
         print str(e)
 
+
+def get_meeting_location(meetingID):
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+
+        meetingID = (meetingID,)
+
+        c.execute("SELECT meetingLocationLatitude, meetingLocationLongitude FROM meetingTab WHERE meetingID=?", meetingID)
+        meetingLocation = c.fetchone()
+        conn.close()
+        print(meetingLocation)
+
+        return meetingLocation
+    except Exception as e:
+        print str(e)
+
+
+def get_meeting_groupID(meetingID):
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+
+        meetingID = (meetingID,)
+
+        c.execute("SELECT groupID FROM meetingTab WHERE meetingID=?", meetingID)
+        groupID = c.fetchone()[0]
+        conn.close()
+        print(groupID)
+
+        return groupID
+    except Exception as e:
+        print str(e)
+
+def get_meeting_peopleID(meetingID):
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+
+        meetingID = (meetingID, )
+        c.execute("SELECT peopleID FROM inTab WHERE meetingID=?", meetingID)
+        peopleID = c.fetchall()
+        peopleIDList = []
+        for people in peopleID:
+            peopleIDList.append(people[0])
+        conn.close()
+        print(peopleIDList)
+
+        return peopleIDList
+    except Exception as e:
+        print (str(e))
+
+
+def get_isLate(meetingID, peopleID):
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+
+        param = (meetingID, peopleID)
+        c.execute("SELECT isLate FROM inTab WHERE meetingID=? AND peopleID=?", param)
+        isLate = c.fetchone()[0]
+        conn.close()
+        if isLate == 1:
+            print (isLate)
+            return True
+        else:
+            print (isLate)
+            return False
+
+    except Exception as e:
+        print (str(e))
+
+
+def update_isLate(meetingID, peopleID, isLate):
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        if isLate:
+            late = 1
+        else:
+            late = 0
+
+        param = (late, meetingID, peopleID)
+        c.execute("UPDATE inTab SET isLate=? WHERE meetingID=? AND peopleID=?", param)
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
+
+
 # create_db()
 # createMeeting(1,1,1,1,[1,2,3,4])
-# getMeetingTime(2)
+# get_meeting_time(2)
+# get_meeting_location(2)
+# get_meeting_groupID(2)
+# get_meeting_peopleID(2)
+# get_isLate(2,2)
+# update_isLate(2,2,True)
+# get_isLate(2,2)
