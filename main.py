8fd5190ee67818a5eb73d06b45ab259046ea6df4
Ju <jluuiss@gmail.com>
2018-09-08 20:21:45 +0800

    Add handler for ping users

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from telegram.ext import Updater
 from credentials import api_key
 from helpers import *
 from add_meeting import *
+from ping_users import user_handler
+
 bot = get_bot()
 updater = Updater(token=api_key)
 dispatcher = updater.dispatcher
 
 # Add Meeting handler
 dispatcher.add_handler(add_meeting_handler)
+
+# Add User handler
+dispatcher.add_handler(user_handler)
 
 # Start up
 updater.start_polling()
--- ping_users.py
+++ ping_users.py
@@ -2,6 +2,8 @@ import db
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Filters
 
+user_handler = MessageHandler(Filters.location, callback=store_location)
+
 def ping_for_loc(bot, meeting_id):
     people_ids = db.get_users_for_meeting(meeting_id)
     for person_id in people_ids:
@@ -13,10 +15,9 @@ def send_ping_to_person(dispatcher, user_id, meeting_id):
     location_button = KeyboardButton("Send Location", request_location=True)
     msg = "Where u @ my nibba?"
     bot.send_message(user_id, msg, reply_markup=ReplyKeyboardMarkup([location_button]))
-    userHandler = MessageHandler(Filters.location, callback=lambda bot, update: store_location(chat_id, update.message)))
 
-def store_location(user_id, message, meeting_id):
-    if chat_id != update.message.chat_id
-        return
+def store_location(bot, update):
+    user_id = update.message.from_user.id
+    meeting_id = db.get_latest_meeting(user_id)
     db.store_location(user_id, message, meeting_id, message.location)
 
