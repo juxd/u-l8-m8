6745a3f3f833d199ec774baf500be0f20db3caf2
Ju <jluuiss@gmail.com>
2018-09-08 20:29:53 +0800

    Rename functions in ping_users

--- ping_users.py
+++ ping_users.py
@@ -1,13 +1,13 @@
-import db
+import db_manager
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Filters
 
 user_handler = MessageHandler(Filters.location, callback=store_location)
 
 def ping_for_loc(bot, meeting_id):
-    people_ids = db.get_users_for_meeting(meeting_id)
+    people_ids = db_manager.get_meeting_people_id(meeting_id)
     for person_id in people_ids:
-        user_id = db.get_user_id(person_id)
+        user_id = db_manager.get_meeting_id(person_id)
         dispatcher_for_user = Dispatcher(bot)
         send_ping_to_person(dispatcher_for_user, user_id, meeting_id)
 
@@ -18,6 +18,6 @@ def send_ping_to_person(dispatcher, user_id, meeting_id):
 
 def store_location(bot, update):
     user_id = update.message.from_user.id
-    meeting_id = db.get_latest_meeting(user_id)
-    db.store_location(user_id, message, meeting_id, message.location)
+    meeting_id = db_manager.get_latest_meeting(user_id)
+    db_manager.store_location(user_id, message, meeting_id, message.location)
 
