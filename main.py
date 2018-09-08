758100b4f543f04c3a99ecc2d0e58eb0c24207d7
micklwl <micklwl@hotmail.com>
2018-09-08 18:48:57 +0800

    location coord

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ from telegram.ext import MessageHandler, CommandHandler, Filters
 
 def add_input(bot, update):
     event_name = update.message.text.replace("/add ", "")
-    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's date in this format: DD/MM/YY",
+    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's date in this format: DDMMYY",
                     parse_mode="Markdown")
-    dispatcher.add_handler(date_handler)
 
 def collect_date(bot, update):
     date = update.message.text
-    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's time in this format: HH:MM",
+    dispatcher.remove_handler(dateHandler)
+    bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the meeting's time in this format: HHMM",
                 parse_mode="Markdown")
-    dispatcher.add_handler(time_handler)
     
 def collect_time(bot, update):
     time = update.message.text
+    dispatcher.remove_handler(timeHandler)
     bot.send_message(chat_id=update.message.chat_id, text = "Reply this message with the location of meeting",
                 parse_mode="Markdown")
     dispatcher.add_handler(location_handler)
 
 def collect_location(bot, update):
     #somehow get one location
-    location = update.message.location 
+    long = update.message.location.longitude
+    lan = update.message.location.latitude
 
 # Set up bot to start listening.
 bot = get_bot()
