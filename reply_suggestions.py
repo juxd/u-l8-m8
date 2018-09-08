b96bc8325c6fae149c25594182087adf7002e6ba
luwu <luwu@paypal.com>
2018-09-09 02:45:02 +0800

    complete the reply suggestion

--- reply_suggestions.py
+++ reply_suggestions.py
@@ -1,9 +1,32 @@
+import db_manager
+from scheduler import schedule_meeting_reminder\
+
+
 def reply_suggestions(bot, meeting_id):
-    users = getUsersInMeeting(meeting_id)
-    meeting_time = getMeetingTime(meeting_id)
-    on_time_list = [isOnTime(u, meeting_id) for u in users] 
-    ratio_on_time = len(filter(lambda x: x == True, on_time_list)) / len(on_time_list)
+    users = db_manager.get_meeting_username(meeting_id)
+    group_id = db_manager.get_meeting_group_id(meeting_id)
+
+    on_time_list = [db_manager.get_is_late(meeting_id, u) for u in users]
+    ratio_on_time = len(filter(lambda x: x == False, on_time_list)) / len(on_time_list)
     if ratio_on_time > 0.5:
-       # Send group message that most people are on time. 
+       # Send group message that most people are on time.
+        message = "Most of the people are on time, YAY! The meeting is happening!"
+        bot.send_message(chat_id=group_id, text=message)
+
     else :
-       # Send group message that they should reschedule. 
+       # Send group message that they should reschedule.
+        message = "Most of the people cannot make it. let us postpone the meeting"
+        bot.send_message(chat_id=group_id, text=message)
+        reschedule_meeting(bot, meeting_id)
+
+
+def reschedule_meeting(bot, meeting_id):
+    group_id = db_manager.get_meeting_group_id(meeting_id)
+    location = db_manager.get_meeting_location(meeting_id)
+    original_time = db_manager.get_meeting_time(meeting_id)
+    new_time = original_time + 60 * 60
+    usernames = db_manager.get_meeting_username(meeting_id)
+    new_meeting_id = db_manager.create_meeting(group_id, new_time, location[0], location[1], usernames)
+    schedule_meeting_reminder(bot, meeting_id, new_time)
+    bot.send_message(chat_id=group_id, text="meeting rescheduled to one hour later")
+
