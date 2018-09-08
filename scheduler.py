cd264fec4af7752c79e8b663ac1b28565699242d
remo5000 <vigneshshankar@gmail.com>
2018-09-09 05:37:55 +0800

    Use fake time (like in demo)

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,17 @@
-import sched, datetime
+import sched, datetime, time
 from ping_users import ping_for_loc
 from reply_suggestions import reply_suggestions
 from apscheduler.schedulers.background import BackgroundScheduler
+
 
 s = BackgroundScheduler()
 s.start()
 
 def schedule_meeting_reminder(bot, meeting_id, time):
     print("scheduling")
-    thirty_mins_before = time - 60 * 30
+    thirty_mins_before = int(time.time()) + 5
 
-    twenty_five_mins_before = time - 60 * 25
+    twenty_five_mins_before = int(time.time()) + 30
+    print('dt:', datetime.datetime.fromtimestamp(thirty_mins_before))
     job = s.add_job(ping_for_loc, trigger='date', args=(bot, meeting_id), run_date=datetime.datetime.fromtimestamp(thirty_mins_before))
     job = s.add_job(reply_suggestions, trigger='date', args=(bot, meeting_id), run_date=datetime.datetime.fromtimestamp(twenty_five_mins_before))
