29e3e3516016ab7e381cbba850666bd95a65d50c
remo5000 <vigneshshankar@gmail.com>
2018-09-09 20:21:44 +0800

    Minor improvements and bug fixes

--- credentials.py
+++
@@ -1,2 +0,0 @@
-api_key = '<your api key here :)>'
-city_mappers_api_key = '<your api key here>'
--- helpers.py
+++
@@ -1,9 +0,0 @@
-import telegram
-from credentials import api_key
-
-def get_bot():
-    bot = telegram.Bot(token=api_key)
-    return bot
-
-def test_bot(user_id):
-    get_bot().send_message(chat_id=user_id, text='Hello World!')
